f6a70b1ecc72956f609f599df2b38534a11ed576
Владислав Прокудин <prokudin@glosav.ru>
2019-02-01 11:37:10 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Vk-group-statistics
---
+++ app/src/main/java/com/github/tubus/vkgroupstatistics/ActionChoiceActivity.java
@@ -0,0 +1,13 @@
+package com.github.tubus.vkgroupstatistics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActionChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action_choice);
+    }
+}
